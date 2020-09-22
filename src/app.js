b5a89137cf94c84b080df52cff4442304c657992
Diwo777 <diego.zegarra79@icloud.com>
2020-09-21 23:28:08 -0400

    added html

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 const { response } = require('express')
+const path = require('path')
 const express = require('express')
 
+
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-    res.send('<h1> Weather </h1>')
-})
 
-app.get('/help', (req, res) => {
-    res.send([{name: 'Diego'},{name: 'Sarah'}])
-})
+app.use(express.static(publicDirectoryPath))
 
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
+
+// app.get('/help', (req, res) => {
+//     res.send([{name: 'Diego'},{name: 'Sarah'}])
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About</h1>')
+// })
 
 app.get('/weather', (req, res) => {
     res.send({
