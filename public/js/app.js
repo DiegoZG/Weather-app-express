0768ad87a5488fd6b7ed666cfa26e3f725b547c2
Diwo777 <diego.zegarra79@icloud.com>
2020-09-22 22:07:06 -0400

    added style

--- public/js/app.js
+++ public/js/app.js
@@ -8,13 +8,15 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
+    messageOne.textContent = 'Loading....' 
+    messageTwo.textContent = ''
+
    
     fetch('http://localhost:3000/weather?address=' + location)
     .then(res => res.json() )
     .then( data => {
     if(data.error){
-        
-        console.log(data.error)
+        messageOne.textContent = data.error
     } else {
         messageOne.textContent = data.location 
         messageTwo.textContent = data.forecast
